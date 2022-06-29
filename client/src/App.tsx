fa6b6ee8e943694ea28ffa640b8ccc4939019546
ShineTiger <555ysng@gmail.com>
2022-06-29 23:21:17 +0900

    :heavy_plus_sign: tailwind 설치와 daisy ui 설치
    - tailwind를 설치하고
    - 관련 세팅 config 파일
    - daisy ui 테마 적용
    - 적용 잘 되는 거 확인함!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,27 @@ import Main from './pages/Main';
 function App() {
   return (
     <BrowserRouter>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="w-full bg-gray-800 px-4">sdf</div>
+      <button className="btn">Hello daisyUI</button>
+      <div className="alert shadow-lg">
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="stroke-info flex-shrink-0 w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+          <span>12 unread messages. Tap to see.</span>
+        </div>
+      </div>
       <Switch>
         <Route path="/AddMeal" component={AddMeal} />
         <Route path="/Main" component={Main} />
