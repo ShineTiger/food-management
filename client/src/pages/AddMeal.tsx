acce8448baab6e058e094ebb2f6b84525bd744ea
ShineTiger <shinetiger.dev@gmail.com>
2022-07-25 20:39:38 +0900

    :sparkles: 자동완성 더미데이터 리스트 추가

--- client/src/pages/AddMeal.tsx
+++ client/src/pages/AddMeal.tsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddMeal = () => {
+  const [search, setSearch] = useState('');
+
+  const handleInputValue = e => {
+    setSearch(e.target.value);
+  };
+
+  interface dummyNames {
+    id: number;
+    name: string;
+  }
+
+  //dummyData 타입지정
+  const dummyData: Array<dummyNames> = [
+    { id: 1, name: '바밤바' },
+    { id: 2, name: '밥' },
+    { id: 3, name: '밤밥' },
+    { id: 4, name: '보리밥' },
+    { id: 5, name: '자라탕' },
+    { id: 6, name: '장조림' },
+  ];
+
+  const [autoComplete, setAutoComplete] = useState(dummyData);
+
   return (
     <>
       <div className="px-2.5">
@@ -9,7 +32,17 @@ const AddMeal = () => {
             type="text"
             placeholder="Search"
             className="input input-bordered"
+            onChange={handleInputValue}
           />
+        </div>
+        <div>
+          <ul>
+            {autoComplete.map(i => (
+              <li key={i.id}>
+                <label>{i.name}</label>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </>
