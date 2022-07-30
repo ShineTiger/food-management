466f24b988ff3995cd8d9191ec46df640485e3b5
ShineTiger <shinetiger.dev@gmail.com>
2022-07-30 23:12:34 +0900

    :sparkles: 한글 자동완성기능 구현
    # Conflicts:
    #	client/src/pages/AddMeal.tsx

--- client/src/pages/AddMeal.tsx
+++ client/src/pages/AddMeal.tsx
@@ -1,46 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { getRegExp } from 'korean-regexp';
+import e from 'express';
 
 const AddMeal = () => {
-  /*
-  State) input value / type : string / event : onChange
-  State) 부가기능 유무 boolean 
-  State) 자동완성박스 안에 들어가는 값 / type : array
-  style : input value의 length만큼 자동완성박스(array) 안의 요소를 참,거짓을 판별한다 || dummyData의 name을 .split("",1)한다 > (split된 더미데이터, 0).includes(e.target.value) > true
-  */
-  interface dummyNames {
-    id: number;
-    name: string;
-  }
-
-  //dummyData 타입지정
-  const dummyData: Array<dummyNames> = [
-    { id: 1, name: '바밤바' },
-    { id: 2, name: '밥' },
-    { id: 3, name: '밤밥' },
+  const dummyData = [
+    { id: 1, name: '바나나' },
+    { id: 2, name: '바닐라라떼' },
+    { id: 3, name: '바나나우유' },
     { id: 4, name: '보리밥' },
     { id: 5, name: '자라탕' },
     { id: 6, name: '장조림' },
   ];
 
-  //dummyData의 name의 값을 배열로만듬
-  const makeDataArray = dummyData.map(i => i.name);
-
   const [serachInput, setSearchInput] = useState<string | RegExp>('');
-  const [isCompleteBoxOn, setIsCompleteBoxOn] = useState(false);
+  const [isCompleteBox, setIsCompleteBox] = useState(false);
   const [completeList, setCompleteList] = useState(dummyData);
 
-  const handleInputValue = e => {
+  const handleInputValue = (e: { target: { value: string } }) => {
     const getKorean: RegExp = getRegExp(e.target.value);
     setSearchInput(getKorean);
-    //setSearchInput(e.target.value);
   };
 
   const handleCompleteList = () => {
-    if (serachInput !== '' || makeDataArray.includes(serachInput)) {
-      setIsCompleteBoxOn(true);
-      const matchTextList = makeDataArray.filter(text =>
-        text.match(serachInput),
+    if (serachInput === '') {
+      setIsCompleteBox(false);
+    } else {
+      setIsCompleteBox(true);
+      const matchTextList = dummyData.filter(text =>
+        text.name.match(serachInput),
       );
       setCompleteList(matchTextList);
     }
@@ -62,6 +49,23 @@ const AddMeal = () => {
             onChange={handleInputValue}
           />
         </div>
+        {isCompleteBox && (
+          <div className="dropdown">
+            <ul className="menu p-2 shadow bg-base-100 rounded-box w-52">
+              {completeList.map(item => {
+                const itemNameStirng = item.name.match(serachInput);
+                return (
+                  <li key={item.id}>
+                    <a>
+                      <span className="text-orange-500">{itemNameStirng}</span>
+                      {item.name.replace(serachInput, '')}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
       </div>
     </>
   );
