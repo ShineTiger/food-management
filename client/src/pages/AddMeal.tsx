2bf59c41b4469c2c76bcf5377eed616d3cc6d312
ShineTiger <shinetiger.dev@gmail.com>
2022-08-02 18:37:18 +0900

    :bug: 자동완성리스트 매칭 오류 수정
    serachInput의 글자를 적으면 시작글자부터 매칭되지 않는 오류를 수정했습니다

--- client/src/pages/AddMeal.tsx
+++ client/src/pages/AddMeal.tsx
@@ -1,12 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { getRegExp } from 'korean-regexp';
-import e from 'express';
 
 const AddMeal = () => {
-  const dummyData = [
-    { id: 1, name: '바나나' },
-    { id: 2, name: '바닐라라떼' },
-    { id: 3, name: '바나나우유' },
+  /*
+  State) input value / type : string / event : onChange
+  State) 부가기능 유무 boolean 
+  State) 자동완성박스 안에 들어가는 값 / type : array
+  style : input value의 length만큼 자동완성박스(array) 안의 요소를 참,거짓을 판별한다 || dummyData의 name을 .split("",1)한다 > (split된 더미데이터, 0).includes(e.target.value) > true
+  */
+  interface dummyNames {
+    id: number;
+    name: string;
+  }
+
+  //dummyData 타입지정
+  const dummyData: Array<dummyNames> = [
+    { id: 1, name: '바밤바' },
+    { id: 2, name: '밥' },
+    { id: 3, name: '밤밥' },
     { id: 4, name: '보리밥' },
     { id: 5, name: '자라탕' },
     { id: 6, name: '장조림' },
@@ -17,7 +28,10 @@ const AddMeal = () => {
   const [completeList, setCompleteList] = useState(dummyData);
 
   const handleInputValue = (e: { target: { value: string } }) => {
-    const getKorean: RegExp = getRegExp(e.target.value);
+    const getKorean: RegExp = getRegExp(e.target.value, {
+      initialSearch: true,
+      startsWith: true,
+    });
     setSearchInput(getKorean);
   };
 
