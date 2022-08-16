b77195a65b114d7793d39bdcd310fccf0e043822
ShineTiger <94972549+ShineTiger@users.noreply.github.com>
2022-08-16 22:12:57 +0900

    :recycle: 분리된 컴포넌트에 맞게 정규표현식 라이브러리 코드 수정 (#20)

--- client/src/pages/AddMeal.tsx
+++ client/src/pages/AddMeal.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import addmealCss from './AddMeal.module.css';
+import { getRegExp } from 'korean-regexp';
 
 const AddMeal = () => {
   const dummyData = [
@@ -14,36 +15,31 @@ const AddMeal = () => {
   ];
 
   const [searchInputValue, setsearchInputValue] = useState<string>(''); //검색창에 들어가는 값
+  const [regexValue, setRegexValue] = useState<RegExp>(); //검색창에 들어가는 값을 정규식으로 변환해서 들어가는 값(라이브러리 이용할때 씀)
 
   const handleInputValue = (e: { target: { value: string } }) => {
+    const getKorean: RegExp = getRegExp(e.target.value, {});
+    setRegexValue(getKorean);
     setsearchInputValue(e.target.value);
   };
 
-
-  const handleCompleteList = () => {
-    if (stringValue === '') {
-      setIsCompleteBox(false);
-    } else {
-      setIsCompleteBox(true);
-
-      const matchTextList = dummyData.filter(
-        text => regexValue && text.name.match(regexValue.source),
-      );
-
-      setCompleteList(matchTextList);
-    }
-  };
-
+  const CompleteBox = () => {
+    const matchTextList = dummyData.filter(
+      text => regexValue && text.name.match(regexValue.source),
+    );
 
     return (
       <>
         <ul className="menu bg-base-100 rounded-box">
           {matchTextList.map(item => {
+            const complateListRegex =
+              regexValue && item.name.match(regexValue.source);
+            const activeText = complateListRegex && complateListRegex[0];
             return (
               <li key={item.id}>
                 <a className={addmealCss.nogap}>
-                  <span className="text-orange-500">{searchInputValue}</span>
-                  {item.name.replace(searchInputValue, '')}
+                  <span className="text-orange-500">{activeText}</span>
+                  {activeText && item.name.replace(activeText, '')}
                 </a>
               </li>
             );
@@ -64,28 +60,7 @@ const AddMeal = () => {
             onChange={handleInputValue}
           />
         </div>
-
-        {isCompleteBox && (
-          <div className="dropdown">
-            <ul className="menu p-2 shadow bg-base-100 rounded-box w-52">
-              {completeList.map(item => {
-                // 정규표현식에 통과되는 텍스트를 색칠하는 로직
-                const complateListRegex =
-                  regexValue && item.name.match(regexValue.source);
-                const activeText = complateListRegex && complateListRegex[0];
-                return (
-                  <li key={item.id}>
-                    <a className={addmealCss.nogap}>
-                      <span className="text-orange-500">{activeText}</span>
-                      {activeText && item.name.replace(activeText, '')}
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
-
+        <div className="mt-4">{searchInputValue && <CompleteBox />}</div>
       </div>
     </div>
   );
