a28fedf95e0e36da3d7413dd563c4de408b1ba59
Revi-Universe <dldnwn99@gmail.com>
2022-08-24 21:34:50 +0900

    :construction: 모든 음식이름 반환하는 API 임시로 작성

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require('express');
 const { MongoClient } = require("mongodb");
+const { keyChange } = require('./dataAlias');
 
 const port = process.env.SERVER_PORT;
 const app = express();
@@ -9,6 +10,13 @@ const uri = process.env.DB_URI;
 const client = new MongoClient(uri);
 const db = client.db(process.env.DB_NAME);
 const foodData = db.collection(process.env.DB_COLL_FOOD);
+
+app.post('/getFoodNamesAll', async function (req, res) {
+
+    const result = (await foodData.find().toArray()).map(i => i.식품명);
+    console.log(result);
+    res.send(result);
+});
 
 
 app.listen(port, () => {
