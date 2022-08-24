7ae404e16341237a9bf1b874fe8a367e72bf803b
Revi-Universe <dldnwn99@gmail.com>
2022-08-24 21:31:28 +0900

    :sparkles: 환경변수 사용 및 데이터베이스 연결
    환경변수 파일은 저장소에 올라가지 않으므로 별도로 받아야 한다!!

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
+require("dotenv").config();
 const express = require('express');
+const { MongoClient } = require("mongodb");
+
+const port = process.env.SERVER_PORT;
 const app = express();
 
-const port = 5000;
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri);
+const db = client.db(process.env.DB_NAME);
+const foodData = db.collection(process.env.DB_COLL_FOOD);
+
+
 app.listen(port, () => {
                     console.log(`${port} hello server`);
 });
