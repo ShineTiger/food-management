fef0c44a612fd2a4587a4ddcb22bc433651acd21
소이 <100299692+hamsoter@users.noreply.github.com>
2022-08-27 20:06:27 +0900

    🔧 음식 더미데이터 선언 및 타입 지정
    - src/resource 경로에 Dummy.ts 파일을 생성했습니다
    - 음식 리스트 더미 선언 후 모듈로 export
    - 그에 걸맞는 타입을 지정했습니다!

--- client/src/types/food.d.ts
+++ client/src/types/food.d.ts
@@ -3,3 +3,19 @@ interface FoodNameType {
   id: number;
   name: string;
 }
+
+
+interface Food {
+  _id: string;
+  name: string;
+  servingSize: number;
+  kiloCalories: number;
+  water: number; // g
+  protein: number; // g
+  fat: number; // g
+  sodium: number; // mg
+  carbohydrate: number; // g
+  sugars: number; // g
+  saturatedFattyAcid: number; // g
+  caffeine: number | null; // mg
+}
