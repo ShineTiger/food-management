5649855824c96053597e05de7f5a5169f0550356
ShineTiger <shinetiger.dev@gmail.com>
2022-08-30 21:38:45 +0900

    :sparkles: 로그인 유효성검사 추가

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,66 +1,86 @@
-import { useState, useRef } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
+interface LoginFormType {
+  id: string;
+  pw: string;
+}
 const Login = () => {
-  const [enteredId, setEnteredId] = useState('');
-  const [enteredPw, setEnteredPw] = useState('');
-  const [enteredNick, setEnteredNick] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    watch,
+  } = useForm<LoginFormType>();
 
-  const inputChangeIdHandler: React.ChangeEventHandler<
-    HTMLInputElement
-  > = e => {
-    setEnteredId(e.target.value);
-  };
+  const IdPwRegex = /^[a-zA-Z0-9]+$/;
 
-  const inputChangePwHandler: React.ChangeEventHandler<
-    HTMLInputElement
-  > = e => {
-    setEnteredPw(e.target.value);
-  };
-
-  const inputChangeNickHandler: React.ChangeEventHandler<
-    HTMLInputElement
-  > = e => {
-    setEnteredNick(e.target.value);
-  };
-
-  const submitFormHandler = () => {
-    fetch('주소', {
-      method: 'POST',
-      body: JSON.stringify({
-        id: enteredId,
-        pw: enteredPw,
-      }),
-    })
-      .then(resonse => resonse.json())
-      .then(result =>
-        result.token ? alert('로그인 성공') : alert(result.message),
-      );
+  const onPwConfirm: SubmitHandler<LoginFormType> = userdata => {
+    console.log(userdata);
   };
 
   return (
-    <form onSubmit={submitFormHandler}>
-      <h2 className="text-2xl py-3 leading-10 font-medium">로그인</h2>
-      <input
-        id="inputId"
-        type="text"
-        placeholder="아이디"
-        className="input input-bordered w-full"
-        onChange={inputChangeIdHandler}
-        required
-      />
-      <input
-        id="inputId"
-        type="text"
-        placeholder="비밀번호"
-        className="input input-bordered w-full my-2"
-        onChange={inputChangeNickHandler}
-        required
-      />
+    <>
+      <form onSubmit={handleSubmit(onPwConfirm)}>
+        <h2 className="text-2xl py-3 leading-10 font-medium">회원가입</h2>
+        <input
+          {...register('id', {
+            required: '아이디를 입력해 주세요',
+            minLength: {
+              value: 4,
+              message: '4자 이상 입력해 주세요',
+            },
+            maxLength: {
+              value: 15,
+              message: '15자 이하 입력해 주세요',
+            },
+            pattern: {
+              value: IdPwRegex,
+              message: '아이디는 알파벳과 숫자로만 입력할 수 있습니다',
+            },
+          })}
+          placeholder="아이디"
+          className="input input-bordered w-full"
+        />
 
-      <button className="btn btn-block mt-4" type="submit">
-        로그인
-      </button>
-    </form>
+        <label className="label">
+          <span className="label-text-alt text-red-600">
+            {errors.id && errors.id.message && errors.id.message}
+          </span>
+        </label>
+
+        <input
+          type="password"
+          {...register('pw', {
+            required: '비밀번호를 입력해 주세요',
+            minLength: {
+              value: 4,
+              message: '4자 이상 입력해 주세요',
+            },
+            maxLength: {
+              value: 15,
+              message: '15자 이하 입력해 주세요',
+            },
+            pattern: {
+              value: IdPwRegex,
+              message: '비밀번호는 알파벳과 숫자로만 입력할 수 있습니다',
+            },
+          })}
+          placeholder="비밀번호"
+          className="input input-bordered w-full"
+        />
+
+        <label className="label">
+          <span className="label-text-alt text-red-600">
+            {errors.pw && errors.pw.message && errors.pw.message}
+          </span>
+        </label>
+
+        <button className="btn btn-block mt-4" type="submit">
+          로그인
+        </button>
+      </form>
+    </>
   );
 };
 
