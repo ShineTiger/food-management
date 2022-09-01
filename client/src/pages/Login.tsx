6fd0ad06b185072130e963bfc872c68b916e6795
ShineTiger <shinetiger.dev@gmail.com>
2022-09-01 20:33:24 +0900

    :sparkles: 로그인 폼 및 http요청 기능 제작

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 interface LoginFormType {
   id: string;
@@ -16,13 +18,36 @@ const Login = () => {
   const IdPwRegex = /^[a-zA-Z0-9]+$/;
 
   const onVaild: SubmitHandler<LoginFormType> = userdata => {
-    console.log(userdata);
+    const navigate = useNavigate();
+
+    //로그인 폼 전달
+    axios
+      .post('http://localhost:5000/testRegster123', { userdata })
+      .then(response => {
+        if (
+          response.data.isCorrectId === true &&
+          response.data.isCorrectPw === true
+        ) {
+          {
+            navigate('/Main');
+          }
+        } else if (response.data.isCorrectId === false) {
+          //비정상적인 접근으로 가입했을 때 메시지를 읽음, 메세지 종류에 따라서 유저에게 보여주는것이 다르다
+          //if(response.data.message==='') {
+          // return alert('')
+          //}
+          return alert(response.data.message);
+        }
+      })
+      .catch(error => {
+        alert(`${error} '원인을 알 수 없는 오류가 발생했습니다.'`);
+      });
   };
 
   return (
     <>
       <form onSubmit={handleSubmit(onVaild)}>
-        <h2 className="text-2xl py-3 leading-10 font-medium">회원가입</h2>
+        <h2 className="text-2xl py-3 leading-10 font-medium">로그인</h2>
         <input
           {...register('id', {
             required: '아이디를 입력해 주세요',
