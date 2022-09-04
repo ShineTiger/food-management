9f188c4bdf05bc8699a9cbd9edaf9f45a19cee29
hamsoter <omilzmil0w0@gmail.com>
2022-09-04 20:48:35 +0900

    ✨ 하루 식사 칼로리 계산하여  화면에 렌더

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -2,33 +2,59 @@ import React from 'react';
 import { useState } from 'react';
 import MealList from '../components/Main/MealList';
 import Modal from '../components/Main/Modal';
+import Card from '../components/UI/Card';
 import { dummyFoodList, dummyMeals } from '../resource/Dummy';
 
 const Main = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
   const isEmptyMeal = true; // FIXME: 임시로 상수값 사용
 
   // TODO: API에서 받아온 음식 리스트로 변경
   const [daysMeal, setDaysMeal] = useState<DayMeals>(dummyMeals);
+
+  // 이날 먹은 총합 칼로리 계산
+  let totalKcal = 0;
+  Object.values(daysMeal).forEach(time => {
+    if (!time) return;
+    time.forEach((food: Food) => {
+      totalKcal += food.kiloCalories;
+    });
+
+    totalKcal = Math.round(totalKcal);
+  });
+
+  console.log(totalKcal);
 
   return (
     <>
       <div>
         <div className="card bg-base-100 shadow-xl">
           <div className="card-body ">
-            <h2 className="card-title">Shoes!</h2>
+            <h2 className="card-title">식사</h2>
 
             {/* 음식리스트 */}
             {isEmptyMeal ? (
-              <MealList meals={daysMeal}></MealList>
+              <>
+                <div className="stat">
+                  <div className="stat-value text-3xl text-center">
+                    {`${totalKcal} / 2000`}
+
+                    <span className="text-xl ml-2">kcal</span>
+                  </div>
+                  <div className="stat-actions">
+                    <label
+                      htmlFor="my-modal-4"
+                      className="btn modal-button btn-block"
+                    >
+                      식사 추가
+                    </label>
+                  </div>
+                </div>
+
+                {/* <MealList meals={daysMeal}></MealList> */}
+              </>
             ) : (
               <p>If a dog chews shoes whose shoes does he choose?</p>
             )}
-
-            <label htmlFor="my-modal-4" className="btn modal-button">
-              open modal
-            </label>
 
             <input type="checkbox" id="my-modal-4" className="modal-toggle" />
             <label htmlFor="my-modal-4" className="modal cursor-pointer">
