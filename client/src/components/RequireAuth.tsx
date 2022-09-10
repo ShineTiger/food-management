78c71c2137939632bc5843e74388c4f0ee7c727f
ShineTiger <shinetiger.dev@gmail.com>
2022-09-10 20:44:51 +0900

    :sparkles: 토큰 유무 검사 후 빈 내용 검사 기능 추가
    토큰 유무를 로컬스토리지의 value에 값이 있는지 검사한 후에 띄어쓰기로 된 빈 내용이 있는지 추가로 검사 한 후 요청을 보냅니다.

--- client/src/components/RequireAuth.tsx
+++ client/src/components/RequireAuth.tsx
@@ -1,39 +1,30 @@
 import axios from 'axios';
 import React from 'react';
-import { useLocation, Navigate, Outlet } from 'react-router';
-const TOKEN_KEY = 'token';
+import { Navigate, Outlet } from 'react-router';
 
 const RequireAuth = () => {
-  const location = useLocation();
-  const isLoginedd = (tokenKey: string) => {
+  const TOKEN_KEY = 'token';
+
+  const getTokenValue = (tokenKey: string) => {
     const tokenValue = localStorage.getItem(tokenKey);
     return tokenValue;
   };
 
-  const isLogined = (isLoginedd: string | null) => {
-    let loginResult;
-    if (typeof isLoginedd === 'string' && isLoginedd.length > 0) {
-      return loginResult;
+  const isLogined = (getTokenValue: string | null) => {
+    if (typeof getTokenValue === 'string' && getTokenValue.length > 0) {
+      const userData = axios.post('/api/testSuccess');
+      return userData;
     }
-    return loginResult;
-  };
-
-  const navigateToLogin = () => {
-    return (
-      <>
-        {alert('로그인 해주세요')}
-        <Navigate to="/Login" />
-      </>
-    );
-  };
-
-  const isValidToken = () => {
-    const userData = axios.post('/api/testSuccess', isLogined);
-    return;
   };
 
   return (
-    <>{isLogined(TOKEN_KEY) ? <Outlet /> : <Navigate to="/Login" replace />}</>
+    <>
+      {isLogined(getTokenValue(TOKEN_KEY)) ? (
+        <Outlet />
+      ) : (
+        <Navigate to="/Login" />
+      )}
+    </>
   );
 };
 
