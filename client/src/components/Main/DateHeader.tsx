a94cd0c94b3ed1032373b01269c275a55b7ad2c1
hamsoter <omilzmil0w0@gmail.com>
2022-09-17 02:17:57 +0900

    💄 선택한 날이 오늘인지 검사하여 prev,next버튼 렌더

--- client/src/components/Main/DateHeader.tsx
+++ client/src/components/Main/DateHeader.tsx
@@ -2,25 +2,27 @@ import React, { Dispatch, SetStateAction } from 'react';
 import DatePicker, { registerLocale } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import customStyle from './Custom.module.css';
+import DateButton from './DateButton';
 
 import isSameDay from 'date-fns/isEqual';
 import { ko } from 'date-fns/esm/locale';
 import startOfDay from 'date-fns/startOfDay';
-
 interface DateHeader {
   currentDate: Date;
   setCurrentDate: Dispatch<SetStateAction<Date>>;
 }
 
 const DateHeader = ({ currentDate, setCurrentDate }: DateHeader) => {
+  // 오늘 날짜인지 검사
+  const isToday = isSameDay(currentDate, startOfDay(new Date()));
 
-  // TODO: 오늘 날짜인지 검사
   // - 그에 따라 뒤로가는 버튼 렌더
   // date 한글화
   registerLocale('ko', ko);
 
   return (
     <div className="flex items-center relative">
+      {isToday && <DateButton action={'prev'} />}
       <DatePicker
         className={`btn btn-ghost flex m-3 mx-auto text-red ${customStyle.dateBtn}`}
         selected={currentDate}
@@ -30,6 +32,7 @@ const DateHeader = ({ currentDate, setCurrentDate }: DateHeader) => {
         locale="ko"
         onChange={date => date && setCurrentDate(date)}
       />
+      {!isToday && <DateButton action={'next'} />}
     </div>
   );
 };
