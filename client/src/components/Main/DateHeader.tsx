dcf8d4e62a71c748637b4c11d35a335902a2923f
hamsoter <omilzmil0w0@gmail.com>
2022-09-16 23:43:39 +0900

    ✨ DatePicker 옵션 변경
    - DatePicker 한글화
    - DateFormat 설정 (yy.MM.dd)
    - 미래 날짜 선택 불가능하도록 설정 (maxDate)

--- client/src/components/Main/DateHeader.tsx
+++ client/src/components/Main/DateHeader.tsx
@@ -2,18 +2,23 @@ import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
+import { ko } from 'date-fns/esm/locale';
+
 const DateHeader = () => {
   const [currentDate, setCurrentDate] = useState<Date>(new Date());
 
   // TODO: 오늘 날짜인지 검사
   // - 그에 따라 뒤로가는 버튼 렌더
-
-  // TODO: 날짜 유형 변경, 한글화, custom css
+  // date 한글화
+  registerLocale('ko', ko);
 
   return (
     <div>
       <DatePicker
         selected={currentDate}
+        dateFormat={'yy.MM.dd'}
+        maxDate={new Date()} // 오늘 날짜 이후 선택 불가능
+        locale="ko"
         onChange={date => date && setCurrentDate(date)}
       />
     </div>
