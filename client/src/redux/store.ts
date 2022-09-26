7d6f0c88cc27c3bef7a1c3aa38d9dbfc1d2907ad
hamsoter <omilzmil0w0@gmail.com>
2022-09-27 00:36:15 +0900

    🔧 redux 설정 변경(redux store에 object 저장 가능)
    - datepicker로 date를 저장할 시 date object를 redux에 저장할 수 없어
      직렬화 오류가 발생
    
    - 그를 해결하기 위해 store에 미들웨어 설정 변경

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import dateSlice from './slice/dateSlice';
 import foodNameSlice from './slice/foodNameSlice';
 // ...
@@ -7,6 +7,9 @@ const store = configureStore({
     foodNames: foodNameSlice,
     dateInfo: dateSlice,
   },
+  middleware: getDefaultMiddleware({
+    serializableCheck: false,
+  }),
 });
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
