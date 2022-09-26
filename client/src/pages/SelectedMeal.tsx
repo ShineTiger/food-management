429bd8e26f1ba186b62fb4a0f9810c950a105626
ShineTiger <shinetiger.dev@gmail.com>
2022-09-26 16:33:17 +0900

    :sparkles: AddMeal 페이지에 선택된 아이템을 표시

--- client/src/pages/SelectedMeal.tsx
+++ client/src/pages/SelectedMeal.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
 import { Link } from 'react-router-dom';
 
 const SelectedMeal = () => {
   // AddMeal 페이지에서 했던 post 요청을 get 요청할 수 있으나 redux를 쓰고싶으니까 생략
+
+  const selectedFood: string[] = useSelector(
+    (store: RootState) => store.selectedFoods.checkedInput,
+  );
+
   return (
     <>
       <p>
         <span>총칼로리</span>
-        <span>245kcal</span>
+        <span>{selectedFood}</span>
       </p>
       <ul className="menu bg-base-100 rounded-box">
         <li>
