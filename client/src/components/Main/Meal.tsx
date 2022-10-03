6866e8a23cc469d2ba15ac1ffd9d5a724edb2b6f
hamsoter <omilzmil0w0@gmail.com>
2022-10-03 22:32:43 +0900

    🩹 state에 저장하는 response data 수정

--- client/src/components/Main/Meal.tsx
+++ client/src/components/Main/Meal.tsx
@@ -20,25 +20,25 @@ const Meal = () => {
 
   if (daysMeal) {
     Object.values(daysMeal).forEach(time => {
-      for (const i in time) {
-        if (time[i] === null) return; // null time 건너뛰기
-        time[i].forEach((food: Food) => {
-          totalCalorie += food.kiloCalories; // 모든 칼로리 더하기
-        });
-        totalCalorie = Math.round(totalCalorie);
-      }
+      if (time === null) return; // null time 건너뛰기
+
+      time.forEach((food: Food) => {
+        totalCalorie += food.kiloCalories; // 모든 칼로리 더하기
+      });
+      totalCalorie = Math.round(totalCalorie);
     });
   }
 
   // 최초 1회, currentDate의 식사 목록을 받아오는 api 요청
   useEffect(() => {
     const fetchData = async () => {
-      const response: DayMeals = await apiTest(
+      const response = await apiTest(
         `http://testDomain.io/testing/${dummyCurrentDateString}`,
-        '',
+        { date: dummyCurrentDateString },
         dummyMeals,
       );
-      setDaysMeal(response);
+      console.log(response.data);
+      setDaysMeal(response.data);
     };
     fetchData();
   }, [dummyCurrentDateString]);
