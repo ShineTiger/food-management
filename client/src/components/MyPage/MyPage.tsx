25ad6c4bf7055d3aba0623b07d869c8e4d3a173b
hamsoter <omilzmil0w0@gmail.com>
2022-10-04 21:58:50 +0900

    💄 MyPage UI 추가
    - 로그아웃, 회원탈퇴 버튼 추가
    - 프로필, 유저정보 렌더

--- client/src/components/MyPage/MyPage.tsx
+++ client/src/components/MyPage/MyPage.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { apiTest } from '../../utils/common';
+import Card from '../UI/Card';
+import ProfileAvatar from '../UI/ProfileAvatar';
 
 const MyPage = () => {
   interface UserDataType {
@@ -25,6 +27,29 @@ const MyPage = () => {
     };
     fetchData();
   }, []);
+
+  return (
+    <div>
+      <Card color="bg-white">
+        <div className={'flex justify-center my-10'}>
+          <ProfileAvatar isBigSize={true}></ProfileAvatar>
+          <div className="mx-5 flex flex-col justify-center">
+            <h3>{userData.name}</h3>
+            <span className="text-xs">{userData.id}</span>
+          </div>
+        </div>
+        <ul className="menu bg-base-100 p-2 rounded-box text-center">
+          <div className="divider"></div>
+          <li>
+            <a className="text-center">로그아웃</a>
+          </li>
+          <li>
+            <a className="text-error text-center">회원탈퇴</a>
+          </li>
+        </ul>
+      </Card>
+    </div>
+  );
 };
 
 export default MyPage;
