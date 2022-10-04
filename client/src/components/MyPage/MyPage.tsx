c284c663f970f9b160a9d94e8ab8b22ee5af8e91
hamsoter <omilzmil0w0@gmail.com>
2022-10-04 21:30:53 +0900

    ✨ dummy 로그인 유저 데이터 받아오는 testApi 작성

--- client/src/components/MyPage/MyPage.tsx
+++ client/src/components/MyPage/MyPage.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { apiTest } from '../../utils/common';
 
 const MyPage = () => {
-  return <div>MyPage</div>;
+  interface UserDataType {
+    id: string;
+    name: string;
+  }
+  const dummyUserData = {
+    id: 'Dummy ID',
+    name: 'Dummy Name',
+  };
+
+  const [userData, setUserData] = useState<UserDataType>({ id: '', name: '' });
+
+  // 최초 1회, 유저데이터를 받아옴
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await apiTest(
+        `http://testDomain.io/testing/`,
+        {},
+        dummyUserData,
+      );
+      setUserData(response.data);
+    };
+    fetchData();
+  }, []);
 };
 
 export default MyPage;
