ea8e8f5b110ebeb6919dbc1dbc799da3459a1dec
ShineTiger <shinetiger.dev@gmail.com>
2022-10-08 05:11:30 +0900

    :sparkles: 로그아웃 버튼 클릭시 토큰 삭제
    로컬스토리지를 삭제하고 페이지를 다시 리로드 합니다.

--- client/src/components/MyPage/Menu.tsx
+++ client/src/components/MyPage/Menu.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Menu = () => {
+  const removeToken = () => {
+    localStorage.removeItem('token');
+    window.location.reload();
+  };
+
   return (
     <ul className="menu bg-base-100 p-2 rounded-box text-center">
       <div className="divider"></div>
       <li className="text-center">
-        <Link to={'/login'}>로그아웃</Link>
+        <button onClick={removeToken}>로그아웃</button>
       </li>
       <li className="text-error text-center">
-        <Link to={'/login'}>회원탈퇴</Link>
+        <button>회원탈퇴 문의</button>
       </li>
     </ul>
   );
