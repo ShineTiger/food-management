cd94170ede6189c46493d801f1e4b38474ea277b
ShineTiger <shinetiger.dev@gmail.com>
2022-10-08 21:26:41 +0900

    :lipstick: 로그아웃 버튼 스타일 변경

--- client/src/components/MyPage/Menu.tsx
+++ client/src/components/MyPage/Menu.tsx
@@ -9,10 +9,13 @@ const Menu = () => {
 
   return (
     <ul className="menu bg-base-100 p-2 rounded-box text-center">
-      <div className="divider"></div>
       <li className="text-center">
-        <button onClick={removeToken}>로그아웃</button>
+        <button onClick={removeToken} className="btn btn-outline">
+          로그아웃
+        </button>
       </li>
+      <div className="divider"></div>
+
       <li className="text-error text-center">
         회원탈퇴 문의는
         <br /> shinetiger.dev@gmail.com 으로 부탁드립니다
