8fccc54bfed42b16253d793dbe30a4a9d69bf1b3
Revi-Universe <dldnwn99@gmail.com>
2022-10-21 21:49:00 +0900

    :sparkles: 모든 음식 반환하는 API
    DB 접속 미들웨어도 분리함

---
+++ server/middleware/database.js
@@ -0,0 +1,13 @@
+const { MongoClient } = require("mongodb");
+
+const database = {};
+const uri = process.env.DB_URI;
+database.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+database.client.connect().then(db => console.log("DB connected."));
+
+database.db = database.client.db(process.env.DB_NAME);
+database.session = database.db.collection(process.env.DB_COLL_SESSION);
+database.foodData = database.db.collection(process.env.DB_COLL_FOOD);
+database.userData = database.db.collection(process.env.DB_COLL_USER);
+
+module.exports = database;
